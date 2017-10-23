a1fb8dbd1e4ae9bfafe3ae199c10a2dd573d890d
ZurMaD <pablo.diazv@pucp.edu.pe>
2017-10-23 15:26:55 -0500

    Update enviar.php

--- xampp/htdocs/enviar.php
+++ xampp/htdocs/enviar.php
@@ -1,1 +1,37 @@
-asf
+<?php
+$con = mysqli_connect('ipCloud','root','123456');
+if(!$con)
+{
+ echo 'No se establecio conexión con el servidor\r';
+}
+if (!mysqli_select_db ($con,'sismo1'))
+{
+ echo 'Error al conectar con base de datos\n\r';
+}
+$SismoID = $_GET["id"];
+$acelx = $_GET['x'] ;
+$acely = $_GET['y'] ;
+$acelz = $_GET['z'] ;
+echo $SismoID;
+echo $acelx;
+echo $acely;
+echo $acelz;
+/*
+   $id=isset($_GET['id'])  ;
+    $x=isset($_GET['x']) ;
+    $y=isset($_GET['y']) ;
+    $z=isset($_GET['z']) ;
+*/
+//INSERT INTO `sismo1`.`sismo1_historial` (`SismoID`, `acelx`, `acely`, `acelz`) VALUES ('234', '234234', '23423', '423423');
+//INSERT INTO sismo1_historial (SismoID ,acelx , acely, acelz) VALUES ($SismoID,$acelx, $acely, $acelz)
+$sql = "INSERT INTO `sismo1_historial` (`SismoID`, `acelx`, `acely`, `acelz`) VALUES ($SismoID, $acelx, $acely, $acelz)";
+if (!mysqli_query($con,$sql))
+{
+ echo 'Error al insertar! Quizás sea tabla errónea!';
+}
+else
+{
+ echo 'Insertación perfecta +1!';
+}
+//header("refresh:1; url=pucp.html");
+?>
